8886e156e14d99dc1e103cdfa2d76053f77102e3
imranmaj <49664304+imranmaj@users.noreply.github.com>
2020-08-27 01:11:34 -0700

    Add additional test cases

--- src/lib.rs
+++ src/lib.rs
@@ -9,28 +9,58 @@ pub use websocket::WebSocket;
 
 #[cfg(test)]
 mod tests {
-    #[tokio::test]
-    async fn echo() {
-        use crate::WebSocket;
+    use crate::*;
 
-        WebSocket::connect("ws://echo.websocket.org/")
+    #[tokio::test]
+    async fn echo_length_0_to_125() {
+        let mut ws = WebSocket::connect("ws://echo.websocket.org/")
             .await
             .unwrap();
+        let message = "a".repeat(3).to_string();
+        ws.send_text(message.clone(), false, true).await.unwrap();
+        let received_frame = ws.receive().await.unwrap();
+        let received_message = received_frame.as_text().unwrap().0.clone();
+        assert_eq!(message, received_message);
+    }
+
+    #[tokio::test]
+    async fn echo_length_126_to_u16_max() {
+        let mut ws = WebSocket::connect("ws://echo.websocket.org/")
+            .await
+            .unwrap();
+        let message = "a".repeat(300).to_string();
+        ws.send_text(message.clone(), false, true).await.unwrap();
+        let received_frame = ws.receive().await.unwrap();
+        let received_message = received_frame.as_text().unwrap().0.clone();
+        assert_eq!(message, received_message);
+    }
+
+    #[tokio::test]
+    async fn echo_length_u16_max_to_u64_max() {
+        let mut ws = WebSocket::connect("ws://echo.websocket.org/")
+            .await
+            .unwrap();
+        let message = "a".repeat(66000).to_string();
+        ws.send_text(message.clone(), false, true).await.unwrap();
+        let received_frame = ws.receive().await.unwrap();
+        let received_message = received_frame.as_text().unwrap().0.clone();
+        assert_eq!(message, received_message);
     }
 
     #[tokio::test]
     async fn echo_tls() {
-        use crate::WebSocket;
-
-        WebSocket::connect("wss://echo.websocket.org/")
+        let mut ws = WebSocket::connect("wss://echo.websocket.org/")
             .await
             .unwrap();
+        let message = "a".repeat(66000).to_string();
+        ws.send_text(message.clone(), false, true).await.unwrap();
+        let received_frame = ws.receive().await.unwrap();
+        let received_message = received_frame.as_text().unwrap().0.clone();
+        assert_eq!(message, received_message);
     }
 
     #[tokio::test]
     async fn bad_scheme() {
-        use crate::WebSocket;
-
         let resp = WebSocket::connect("http://echo.websocket.org").await;
         if let Ok(_) = resp {
             panic!("expected to fail with bad scheme");
